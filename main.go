5b97cc43fa6f41b05d11fac4c567fa12b57651fe
Dinesh Kumar <bakageddy@gmail.com>
2023-05-28 10:55:09 +0530

    Test API Calls to pokeapi

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"strings"
+
+	"github.com/carlmjohnson/requests"
 )
 
+type JsonElement = map[string]interface{}
+
+const apiBase string = "https://pokeapi.co/api/v2/"
+
 func main() {
-	fmt.Println("Hello, World!")
+	var contentBody map[string]interface{}
+	ctx := context.Background()
+
+	err := requests.
+		URL(apiBase).
+		Path("pokemon/charizard").
+		ToJSON(&contentBody).
+		Fetch(ctx)
+
+	
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	types := contentBody["types"].([]interface{})
+	for _, elem := range types {
+		x := elem.(JsonElement)
+		pokeInfo := x["type"].(JsonElement)
+		pokeType := pokeInfo["name"].(string)
+		log.Println(strings.Title(strings.ToLower(pokeType)))
+	}
 }
