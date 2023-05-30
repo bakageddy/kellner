0d5cf982768a4493eb359d5b8b83b952b0603d4b
Dinesh Kumar G <gdineshda@gmail.com>
2023-05-30 22:39:30 +0530

    Remove redundant stuff

--- main.go
+++ main.go
@@ -1,37 +1,10 @@
 package main
 
 import (
-	"context"
-	"log"
-	"strings"
-
-	"github.com/carlmjohnson/requests"
+	"fmt"
+	"github.com/gdnand/kellner/pokemon"
 )
 
-type JsonElement = map[string]interface{}
-
-const apiBase string = "https://pokeapi.co/api/v2/"
-
 func main() {
-	var contentBody map[string]interface{}
-	ctx := context.Background()
-
-	err := requests.
-		URL(apiBase).
-		Path("pokemon/charizard").
-		ToJSON(&contentBody).
-		Fetch(ctx)
-
-	
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	types := contentBody["types"].([]interface{})
-	for _, elem := range types {
-		x := elem.(JsonElement)
-		pokeInfo := x["type"].(JsonElement)
-		pokeType := pokeInfo["name"].(string)
-		log.Println(strings.Title(strings.ToLower(pokeType)))
-	}
+	fmt.Println(pokemon.New("pikachu"))
 }
