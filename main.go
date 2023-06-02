396eef1f381694b483b7242b56484b4a9e2331d9
Dinesh Kumar G <gdineshda@gmail.com>
2023-06-02 20:56:11 +0530

    Add: Pokemon Endpoints

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
+// kellner
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	// "io/ioutil"
+	"log"
+	"net/http"
+
 	"github.com/gdnand/kellner/pokemon"
 )
 
+// PokemonHandle for handling /pokemon endpoint
+type PokemonHandle struct{}
+
+// Handle responses to serve pokemon endpoint!
+func (*PokemonHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	enc := json.NewEncoder(w)
+
+	name := r.FormValue("name")
+	pokemon, err := pokemon.New(name)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("Failed to find pokemon: ", r.RequestURI)
+		return
+	}
+
+	enc.Encode(pokemon)
+}
+
 func main() {
-	fmt.Println(pokemon.New("pikachu"))
+	http.Handle("/pokemon", &PokemonHandle{})
+	http.ListenAndServe(":8080", nil)
 }
